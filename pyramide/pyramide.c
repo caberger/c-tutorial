30ae1e49dd58f635ccf1ec362d6a1e3a2cd3f217
Christian Aberger <c.aberger@htl-perg.ac.at>
2023-09-10 09:35:02 +0200

    cmake debugging

--- pyramide/pyramide.c
+++ pyramide/pyramide.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include "pyramide.h"
 
-Pyramide createPyramide() {
-	Pyramide pyramide;
-	pyramide.currentLine = 0;
-	pyramide.currentRow = 0;
-	
-	return pyramide;
+int main(void) {
+    for (int i = 0; i < 10; i++) {
+        printf("hello world! %d\n", i);
+    }
+    return 0;
 }
