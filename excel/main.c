2b2876877a9de18cd909044a797685d6f3ada8ff
Christian Aberger <c.aberger@htl-perg.ac.at>
2023-09-10 17:00:20 +0200

    check max cols

--- excel/main.c
+++ excel/main.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 #define MAX_NAME_LENGTH 80
+#define MAX_NUMBER_OF_COLUMNS 40
 #define MAX_PATH 512
 #define DELIMITER ","
 
@@ -20,7 +21,7 @@ typedef struct
     int height;
 } Person;
 
-Person* parse_person(const char **columns) {
+Person* parse_person(char ** columns) {
     Person* person = calloc(1, sizeof(Person));
     int col = 0;
     strncpy(person->first_name, columns[col++], MAX_NAME_LENGTH);
@@ -28,20 +29,22 @@ Person* parse_person(const char **columns) {
     person->height = atoi(columns[col++]);
     return person;
 }
-
+void print_person(FILE *fp, Person *person) {
+    fprintf(fp, "Person first_name=%s, last_name=%s, height=%d\n", person->first_name, person->last_name, person->height);
+}
 enum ParserState {
     outside, // no '"' found until now
     inside // reading the current string inside the first '"'
 };
-char **parse_csv_line(char *line) {
-    char **fields = calloc(40, sizeof(char *));
+char ** parse_csv_line(const char *line) {
+    char **fields = calloc(MAX_NUMBER_OF_COLUMNS, sizeof(char *));
     char **current_field = fields;
     char *destination_buffer = malloc(strlen(line)); // we remove the "" and set replace the comma by a \0, so that should be enough :)
     enum ParserState state = outside;
     char *current_write_position = destination_buffer; //the location where the next byte is written to.
     const char *reading_position; // the next position we will read from the line.
     int numberOfColumnsFinished = 0;
-    for (const char *reading_position = line; *reading_position; reading_position++) {
+    for (const char *reading_position = line; *reading_position && numberOfColumnsFinished < MAX_NUMBER_OF_COLUMNS; reading_position++) {
         char ch = *reading_position;
         switch(state) {
             case outside:
@@ -70,7 +73,6 @@ char **parse_csv_line(char *line) {
 }
 int main(void)
 {
-
     char *line = NULL;
     long max_path = pathconf(".", _PC_PATH_MAX);
 
@@ -93,9 +95,9 @@ int main(void)
             if (count_of_lines > 0) {
                 char **columns = parse_csv_line(current_buffer);
                 Person* person = parse_person(columns);
+                print_person(stdout, person);
                 free(person), person = NULL;
                 free(columns), columns = NULL;
-                printf("%s\n", current_buffer);
             }
             else {
                 printf("skip header line %s", current_buffer);
