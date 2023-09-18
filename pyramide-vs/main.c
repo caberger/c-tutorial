3c2b6f8932ce0f224e5dc07f6d493cf5e2991120
Christian Aberger <qrzbrz@yahoo.de>
2023-09-18 08:44:04 +0200

    demonstrate memory leak debugging

--- pyramide-vs/main.c
+++ pyramide-vs/main.c
@@ -1,10 +1,21 @@
-
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 #include <stdio.h>
 #include "pyramide.h"
 
 int main() {
 	Pyramide model = createPyramide();
 	model.currentRow = 2;
-
 	printf("Hello World! %dx%d\n", model.currentLine, model.currentRow);
+
+	// This is an intentional memory leak to demonstrate memory debugging
+	const char* text = "not freed !";
+	char *leak = malloc(strlen(text) + 1);
+	strcpy(leak, text);
+
+	// this is intentionally skipped: free(leak);
+
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();
 }
